75d9497666cf63402e4866b2f21ae7bb3fd8aca4
RujutaJ1 <rujuta.jo@gmail.com>
2016-06-03 09:11:31 +0530

    Code for makign the n grams and preparing the n gram files for use in the algorithm.

---
+++ Code/ngramForServer.R
@@ -0,0 +1,55 @@
+library(beepr)
+library(data.table)
+library(tau)
+library(plyr)
+setwd("F:/Rujuta/Coursera/Capstone/ngram2")
+load("allTrigrams_clean.RData")
+load("allBigrams_clean.RData")
+load("allTokens_clean.RData")
+load("allFourgrams_clean.RData")
+load("allTrigrams2_clean.RData")
+load("allBigrams2_clean.RData")
+load("allTokens2_clean.RData")
+load("allFourgrams2_clean.RData")
+
+allBigrams <- rbind(allBigrams,allBigrams2)
+allTrigrams <- rbind(allTrigrams,allTrigrams2)
+allTokens <- rbind(allTokens,allTokens2)
+
+allTrigrams2 <- allTrigrams[count > 2]
+allBigrams2 <- allBigrams[count > 2]
+allTokens2 <- allTokens[count > 2]
+#allFourgrams2 <- allFourgrams[count>2]
+save(allTokens2, file="finalTokens.RData")
+
+#dim(allFourgrams2)
+dim(allTrigrams2)
+dim(allBigrams2)
+dim(allTokens2)
+
+trigrams <- do.call(rbind, strsplit(allTrigrams2$ngram, split = " "))
+trigrams <- cbind(apply(trigrams[, 1:2], 1, function(x) paste(x, collapse = " ")),
+                  trigrams[, 3])
+trigrams <- cbind.data.frame(trigrams, allTrigrams2$count)
+setnames(trigrams, old = c("1","2","allTrigrams2$count"), new = c('ngram',"nextword","count"))
+
+delete <- grep(pattern = "' ", trigrams$ngram)
+trigrams <- trigrams[-delete, ]
+trigrams <- arrange(trigrams, count)
+trigrams <- data.table(trigrams)
+save(trigrams, file="finaltrigrams.RData")
+
+bigrams <- do.call(rbind, strsplit(allBigrams2$ngram, split = " "))
+bigrams <- cbind.data.frame(bigrams, allBigrams2$count)
+setnames(bigrams, old = c("1","2","allBigrams2$count"), new = c('ngram',"nextword","count"))
+
+bigrams <- bigrams[-(which(bigrams$ngram == "'")), ]
+bigrams <- arrange(bigrams, count)
+bigrams <- data.table(bigrams)
+save(bigrams, file="finalbigrams.RData")
+
+setkey(allTokens2, "count", "ngram")
+setkey(bigrams, "count", "ngram")
+setkey(trigrams, "count", "ngram")
+#setkey(fourgrams,"count","ngram")
+
